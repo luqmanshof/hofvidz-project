e2e3ef8de57d8b172d76087d6f244828131243be
Luqman <luqmanshof@gmail.com>
2019-09-25 08:39:47 +0100

    project update

---
+++ halls/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from .models import Hall
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+def home(request):
+    return render(request,'halls/home.html')
+
+def dashboard(request):
+    return render(request,'halls/dashboard.html')
+
+class Signup(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        view = super(Signup, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request,user)
+        return view
+
+
+class CreateHall (LoginRequiredMixin, generic.CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('home')
+
+class DetailHall(generic.DetailView):
+    model = Hall
+    template_name = 'halls/detail_hall.html'
+
+class UpdateHall(generic.UpdateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/update_hall.html'
+    # context = {"form" : NameForm()}
+    success_url = reverse_lazy('dashboard')
+
+class DeleteHall (LoginRequiredMixin, generic.DeleteView):
+    model = Hall
+    template_name = 'halls/delete_hall.html'
+    success_url = reverse_lazy('dashboard')
