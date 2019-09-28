b19fd8955bb4d163755b55082957b8df0f200804
Luqman <luqmanshof@gmail.com>
2019-09-29 00:40:32 +0100

    updated hall templates

--- halls/views.py
+++ halls/views.py
@@ -2,15 +2,77 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
-from .models import Hall
+from .models import Hall,Video
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from halls.forms import VideoForm, SearchForm
+from django.forms import formset_factory
+from django.http import Http404, JsonResponse
+from django.forms.utils import ErrorList
+from django.contrib.auth.decorators import login_required
+import urllib
+import requests
+
+
+YOUTUBE_API_KEY = 'AIzaSyBxpQz7OQ9BvvdbGrKIZ4uRuDGjeZtIKfg'
 
 def home(request):
-    return render(request,'halls/home.html')
+    recent_halls = Hall.objects.all().order_by('-id')[:3]
+    popular_halls = [Hall.objects.get(pk=2),Hall.objects.get(pk=5)]
+    return render(request,'halls/home.html', {'recent_halls':recent_halls, 'popular_halls':popular_halls})
 
 def dashboard(request):
-    return render(request,'halls/dashboard.html')
+    halls = Hall.objects.filter(user=request.user)
+    return render(request,'halls/dashboard.html',{'halls':halls})
+
+def add_video(request, pk):
+    form = VideoForm()
+    search_form = SearchForm()
+    hall = Hall.objects.get(pk=pk)
+    if not hall.user == request.user:
+        raise Http404
+
+    if request.method == 'POST':
+        form = VideoForm(request.POST)
+        if form.is_valid():
+            video = Video()
+            video.hall = hall
+            video.url = form.cleaned_data['url']
+            parsed_url = urllib.parse.urlparse(video.url)
+            video_id  = urllib.parse.parse_qs(parsed_url.query).get('v')
+            if video_id:
+                video.youtube_id = video_id[0]
+                response = requests.get(f'https://www.googleapis.com/youtube/v3/videos?part=snippet&id={ video_id[0] }&key={ YOUTUBE_API_KEY }')
+                json = response.json()
+                title = json['items'][0]['snippet']['title']
+                video.title = title
+                video.save()
+                return redirect('detail_hall',pk)
+            else:
+                errors = form._errors.setdefault('url',ErrorList())
+                errors.append('Needs to be a YouTube Url')
+
+    return render(request,'halls/add_video.html',{'form':form,'search_form':search_form,'hall':hall})
+
+@login_required
+def video_search(request):
+    search_form = SearchForm(request.GET)
+    if search_form.is_valid():
+        encoded_search_term = urllib.parse.quote(search_form.cleaned_data['search_term'])
+        response = requests.get(f'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q={ encoded_search_term }&key={ YOUTUBE_API_KEY }')
+        return JsonResponse(response.json())
+    return JsonResponse({'error':'Not able to validate form'})
+
+class DeleteVideo(LoginRequiredMixin, generic.DeleteView):
+    model = Video
+    template_name = 'halls/delete_video.html'
+    success_url = reverse_lazy('dashboard')
+
+    def get_object(self):
+        video = super(DeleteVideo, self).get_object()
+        if not video.hall.user == self.request.user:
+            raise Http404
+        return video
 
 class Signup(generic.CreateView):
     form_class = UserCreationForm
@@ -34,7 +96,7 @@ class CreateHall (LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         super(CreateHall, self).form_valid(form)
-        return redirect('home')
+        return redirect('dashboard')
 
 class DetailHall(generic.DetailView):
     model = Hall
--- hofvidz/urls.py
+++ hofvidz/urls.py
@@ -33,6 +33,12 @@ urlpatterns = [
     path('halloffame/create',views.CreateHall.as_view(),name='create_hall'),
     path('halloffame/<int:pk>',views.DetailHall.as_view(),name='detail_hall'),
     path('halloffame/<int:pk>/update',views.UpdateHall.as_view(),name='update_hall'),
-    # path('halloffame/<int:pk>/delete',views.DeleteHall.as_view(),name='delete_hall'),
+    path('halloffame/<int:pk>/delete',views.DeleteHall.as_view(),name='delete_hall'),
+
+    #Video
+    path('halloffame/<int:pk>/addvideo',views.add_video,name='add_video'),
+    path('video/search', views.video_search, name='video_search'),
+    path('video/<int:pk>/delete', views.DeleteVideo.as_view(), name='delete_video'),
+
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
